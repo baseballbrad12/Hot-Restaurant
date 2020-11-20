e5ce047fde4edf3f77dce0223c64d09bcb6d4736
Jeremiah Holden <jeremiah@cardconnectcp.com>
2020-11-19 20:33:23 -0800

    changed the tables and waitinglist js files

---
+++ info/tables.js
@@ -0,0 +1,32 @@
+var tables = [
+    {
+      tableNum: `${table1}`,
+      Name: `${name1}`,
+      Email: `${email1}`,
+      Phone: `${phone1}`
+    },
+    {
+      tableNum: `${table2}`,
+      Name: `${name2}`,
+      Email: `${email2}`,
+      Phone: `${phone2}`
+    },
+    {
+      tableNum: `${table3}`,
+      Name: `${name3}`,
+      Email: `${email3}`,
+      Phone: `${phone3}`
+    },
+    {
+      tableNum: `${table4}`,
+      Name: `${name4}`,
+      Email: `${email4}`,
+      Phone: `${phone4}`
+    },
+    {
+      tableNum: `${table5}`,
+      Name: `${name5}`,
+      Email: `${email5}`,
+      Phone: `${phone5}`
+    }
+  ];
